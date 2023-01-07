d79ffe524fa48423b5b91fbb3c990e5bef687d2b
Noah Altunian <nbaltunian@gmail.com>
2023-01-06 23:17:55 -0800

    Update site.location in _config.ts to use Deno.env

--- _config.ts
+++ _config.ts
@@ -2,7 +2,7 @@ import lume from "lume/mod.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog@v0.5.0/mod.ts";
 
 const site = lume({
-  location: new URL("https://altun.cc"),
+  location: new URL(`https://${Deno.env.get("FQDN")}`),
 });
 
 site.use(blog());
